b00af7e1c234761ee190bd0e8feafdc75ee116a6
Morgan-Saturn <morgan.tunteva@proton.me>
2026-07-15 15:39:48 +0200

    parser shows the 6 first results

--- xml_parser.js
+++ xml_parser.js
@@ -1,16 +1,22 @@
 const rssUrl = "proxy.php";
 const parentElement = document.querySelector(".movie_grid");
+const loadMore = document.querySelector('.voir_plus');
+
+let html = ``;
+let currentResultCount = 6;
+
+
 
 fetch(rssUrl)
 .then(response => response.text()) /*response.text convertit le fichier xml en fichier texte.*/
 .then(data => {
     let parser = new DOMParser();
     let xml = parser.parseFromString(data, "application/xml" );
-    console.log(xml);
     let items = xml.querySelectorAll('item');
+    const firstSixResults = Array.prototype.slice.call(xml.querySelectorAll('item'), 0,6);
     let html = `` ;
 
-    items.forEach(el =>{
+    firstSixResults.forEach(el =>{
         html += `
                 <div class='movie'>
                     <img class='movie_img' src='${el.getElementsByTagName('media:thumbnail')[0].getAttribute('url')}' style='width: 100%; height: 100%;' alt='affiche du film'>
@@ -21,5 +27,5 @@ fetch(rssUrl)
                 </div>
                 `
     });
-    parentElement.innerHTML = html;
+        parentElement.innerHTML = html;
 });
