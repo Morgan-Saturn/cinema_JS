fec66d7cd62fc3d44efc793736e67b455d88acd5
Morgan-Saturn <morgan.tunteva@proton.me>
2026-07-15 18:15:40 +0200

    made the load more button functionnal

--- xml_parser.js
+++ xml_parser.js
@@ -1,31 +1,57 @@
 const rssUrl = "proxy.php";
 const parentElement = document.querySelector(".movie_grid");
 const loadMore = document.querySelector('.voir_plus');
+var view = {
+    news : [],
+    currentResultCount : 3
+};
+//une fonction pour load les news, une pour générer l'affichage avec le bouton load more
 
-let html = ``;
-let currentResultCount = 6;
+function loadNews() {
+    fetch(rssUrl + '?count='+ view.currentResultCount)  //reçoit le nb d'items voulu à charger
+    .then(response => response.text())
+    .then(data => {
+        let parser = new DOMParser();
+        let xml = parser.parseFromString(data, "application/xml" );
+        //let items = xml.querySelectorAll('item');
+        let items = Array.from(xml.querySelectorAll('item'));
+        let results = Array.prototype.slice.call(xml.querySelectorAll('item'), 0,view.currentResultCount);
 
+        //... = déploie le tableau et le transforme en n arguments
+        view.news = results.map(function (el) {
 
+            let img = el.getElementsByTagName('media:thumbnail')[0].getAttribute('url');
+            let link = el.querySelector('link').textContent;
+            let title = el.querySelector('title').textContent;
+            let description = el.querySelector('description').textContent
+            return {
+                img, link, title, description
+            }
+        });
+    displayNews();
+    })
+};
+    
+loadNews();
 
-fetch(rssUrl)
-.then(response => response.text()) /*response.text convertit le fichier xml en fichier texte.*/
-.then(data => {
-    let parser = new DOMParser();
-    let xml = parser.parseFromString(data, "application/xml" );
-    let items = xml.querySelectorAll('item');
-    const firstSixResults = Array.prototype.slice.call(xml.querySelectorAll('item'), 0,6);
-    let html = `` ;
-
-    firstSixResults.forEach(el =>{
+function displayNews() {
+    let html = '';
+    view.news.slice(-view.currentResultCount).forEach(item => {
         html += `
                 <div class='movie'>
-                    <img class='movie_img' src='${el.getElementsByTagName('media:thumbnail')[0].getAttribute('url')}' style='width: 100%; height: 100%;' alt='affiche du film'>
+                    <img class='movie_img' src='${item.img}' style='width: 100%; height: 100%;' alt='affiche du film'>
                     <h3 class='categories'>
-                        <a href='${el.querySelector('link').textContent}'>${el.querySelector('title').textContent}</a>
+                        <a href='${item.link}'>${item.title}</a>
                     </h3>
-                    <p class='summary'>${el.querySelector('description').textContent}</p>
+                    <p class='summary'>${item.description}</p>
                 </div>
-                `
-    });
-        parentElement.innerHTML = html;
+                `;
+                
+    })
+    parentElement.innerHTML = html;
+}
+loadMore.addEventListener('click', () => {
+    
+    view.currentResultCount += 3;
+    loadNews();
 });
